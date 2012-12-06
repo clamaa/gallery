deda028202066e8cb8698998f7e69d706591e24d
clamaa <clark.mazhiqiang@gmail.com>
2012-12-06 10:16:04 +0800

    Use Annotation to replace configuration file.

--- gallery-web/src/com/clamaa/gallery/controller/FirstSpringController.java
+++ gallery-web/src/com/clamaa/gallery/controller/FirstSpringController.java
@@ -1,10 +1,13 @@
-package com.clamaa.gallery.contoller;
+package com.clamaa.gallery.controller;
 
 import com.clamaa.gallery.entity.User;
 import com.clamaa.gallery.services.UserServices;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +17,23 @@ import javax.sql.DataSource;
  * @author Clark Ma
  *         created at 11/22/12
  */
-public class FirstSpringController extends AbstractController {
+@Controller
+@RequestMapping("/login.do")
+public class FirstSpringController  {
 
     private Logger logger = Logger.getLogger(getClass());
 
+    @Autowired
     private UserServices userServices;
-    private DataSource dataSource;
-
-    public DataSource getDataSource() {
-        return dataSource;
-    }
-
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+//    private DataSource dataSource;
+//
+//    public DataSource getDataSource() {
+//        return dataSource;
+//    }
+//
+//    public void setDataSource(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     public UserServices getUserServices() {
         return userServices;
@@ -37,7 +43,7 @@ public class FirstSpringController extends AbstractController {
         this.userServices = userServices;
     }
 
-    @Override
+    @RequestMapping
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws
                                                                                                            Exception {
         String username = request.getParameter("username");
--- gallery-web/src/com/clamaa/gallery/services/UserServices.java
+++ gallery-web/src/com/clamaa/gallery/services/UserServices.java
@@ -5,6 +5,7 @@ import com.clamaa.gallery.utils.HibernateUtils;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
  * @author Clark Ma
  *         created at 11/9/12
  */
+@Service
 public class UserServices {
 
     public boolean validateUser(User user){
